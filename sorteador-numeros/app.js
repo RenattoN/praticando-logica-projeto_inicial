0e2a969290a098e8ed18a0c39fc4f091a262d582
RenattoN <76630329+RenattoN@users.noreply.github.com>
2024-07-14 01:26:25 -0300

    segundo commit

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear(params) {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+}
